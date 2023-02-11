a8dd69a4a198a66047e4c248a53aad7689b28efb
Abhinav Saxena <abhinav150601@gmail.com>
2023-02-11 20:30:15 +0530

    added login page

--- vitality/lib/constants/color_constants.dart
+++ vitality/lib/constants/color_constants.dart
@@ -5,4 +5,5 @@ class AppColors {
 
   static const Color blue = Color.fromARGB(255, 92, 10, 224);
   static const Color black = Color.fromARGB(255, 19, 18, 18);
+  static const Color white = Colors.white;
 }
--- vitality/lib/login.dart
+++ vitality/lib/login.dart
@@ -22,7 +22,8 @@ class _loginState extends State<login> {
       backgroundColor: AppColors.black,
       body: SingleChildScrollView(
         child: Column(
-          children: [
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
             Container(
               padding: EdgeInsets.fromLTRB(0, 80, 0, 0),
               alignment: Alignment.center,
@@ -30,6 +31,57 @@ class _loginState extends State<login> {
                 "assets/images/logo.png",
                 height: 250,
                 width: 200,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(50, 0, 0, 0),
+              child: SizedBox(
+                  width: 300,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      suffixIcon: Icon(Icons.person),
+                      border: UnderlineInputBorder(),
+                      labelText: "Enter Username",
+                    ),
+                  )),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(50, 0, 0, 0),
+              child: SizedBox(
+                  width: 300,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      suffixIcon: Icon(Icons.visibility_off),
+                      border: UnderlineInputBorder(),
+                      labelText: "Enter Password",
+                    ),
+                  )),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(150, 0, 0, 0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  alignment: Alignment.center,
+                  primary: AppColors.blue,
+                  onPrimary: AppColors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  'Log In',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
               ),
             ),
           ],
